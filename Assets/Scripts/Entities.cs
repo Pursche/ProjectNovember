fe0fcd806cc1c8b35bb91ec49af5426d43800ebd
Unknown <olssondaniel@live.se>
2016-11-05 22:55:31 +0100

    Add Initial structure of Parts and Actors
    Initial push of the structure WITHOUT scriptable objects.

---
+++ Assets/Scripts/Entities.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Entities
+{
+
+}
+
+public class Android : Entities
+{
+    public string androidID;
+    public string androidName;
+    public List<Part> androidParts = new List<Part>();
+
+    public void EquipItem(string partName)
+    {
+        androidParts.Add(EntryPoint.parts.Find(x => x.partName == partName));
+    }
+}
+
+public class Player : Android
+{
+    public string playerName;
+}
+
+public class Part : Entities
+{
+    public enum enumPartSlot { Weapon, Structural }
+
+    public string partName;
+    public string partID;
+    public enumPartSlot partSlot;
+
+    public float partMass;
+    public float partRating;
+    public float partHitPoints;
+    public float partCoverage;
+
+
+}
+
+public class Power : Part
+{ 
+    public enum partType { }
+
+    public float powerSupply;
+    public float powerStorage;
+    public float genHeat;
+
+}
+
+public class Propulsion : Part
+{
+    public enum partType { Treads, Legs, Wheels, Hover_Unit, Fighter_Unit }
+
+    public partType propulsionType;
+    public float moveModifier;
+    public float powerUsed;
+    public float genHeat;
+}
+
+public class Utility : Part
+{
+    public enum partType { }
+}
+
+public class Weapon : Part
+{
+    public enum partType {
+        Special_Weapon,
+        Melee_Weapon,
+        Ballistic_Guns,
+        Ballistic_Cannon,
+        Energy_Cannons,
+        Energy_Gun,
+        Launcher
+    }
+    public enum attackType { Bashing, Piercing, Slashing }
+    public enum defenceType { Emp, Ballistic, Energy, Thermal }
+
+    // Overview
+    public partType weaponType;
+    public float genHeat;
+
+    // Attack
+    public bool weaponRanged;
+    public float weaponCost; // Redo this with Energy / Matter system?
+    public attackType weaponAttackType;
+
+    // if Ranged
+    public float weaponRecoil;
+    public float weaponTargeting;
+    public float weaponReloadTime; // Delay
+
+    // onHit
+    public float weaponMinAttack;
+    public float weaponMaxAttack;
+    public float weaponCritical;
+
+
+}
+
+public class Manipulation : Part
+{
+    public enum partType { }
+    public float genHeat;
+}
---
+++ Assets/Scripts/EntryPoint.cs
@@ -0,0 +1,156 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EntryPoint : MonoBehaviour {
+
+    #region Public
+    public static List<Part> parts = new List<Part>();
+    #endregion
+
+    #region Private
+
+    private List<Android> androids = new List<Android>();
+    private Player player;
+
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Start!");
+        GenerateObjects();
+        Debug.Log("Amount of parts in list: " + parts.Count);
+        GeneratePlayer();
+        Debug.Log("Player made");
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+
+    // Make all of the parts, FFS put this in a ScriptableObject dude!! 
+    void GenerateObjects()
+    {
+        // --------------------------------------------------------
+        // Initial parts structure totaly 100% riped and taken from
+        // http://gridsagegames.com/wiki/Item
+        // --------------------------------------------------------
+
+        // Starter Deffinitions
+        Power newPower;
+        Propulsion newPropulsion;
+        Utility newUtility;
+        Weapon newWeapon;
+        Manipulation newManipulation;
+
+        #region Power_Systems
+        // ------------ Power System
+        // --- Engines
+        #region New_Item
+        newPower = new Power();
+            newPower.partName = "Standard Engine";
+            newPower.powerStorage = 10;
+            newPower.powerSupply = 2;
+            newPower.genHeat = 1;
+            parts.Add(newPower);
+        #endregion
+
+        // --- Power_cores
+        #region New_Item
+            newPower = new Power();
+            newPower.partName = "Standard Power Core";
+            newPower.powerStorage = 50;
+            newPower.powerSupply = 1;
+            newPower.genHeat = 3;
+            parts.Add(newPower);
+        #endregion
+
+        // --- Reactors 
+        #region New_Item
+            newPower = new Power();
+            newPower.partName = "Standard Reactor";
+            newPower.powerStorage = 25;
+            newPower.powerSupply = 5;
+            newPower.genHeat = 10;
+            parts.Add(newPower);
+        #endregion
+
+        #endregion
+
+        #region Propulsion_Parts
+        // ------------ Propulsion Parts
+        // --- Legs
+        #region New_Item
+            newPropulsion = new Propulsion();
+            newPropulsion.propulsionType = Propulsion.partType.Legs;
+            newPropulsion.partName = "Standard Legs";
+            parts.Add(newPropulsion);
+        #endregion
+
+        // --- Treads
+        #region New_Item
+            newPropulsion = new Propulsion();
+            newPropulsion.propulsionType = Propulsion.partType.Treads;
+            newPropulsion.partName = "Standard Tread";
+            parts.Add(newPropulsion);
+        #endregion
+
+        // --- Wheels 
+        #region New_Item
+            newPropulsion = new Propulsion();
+            newPropulsion.propulsionType = Propulsion.partType.Wheels;
+            newPropulsion.partName = "Standard Wheel";
+            parts.Add(newPropulsion);
+        #endregion
+
+        // --- Fighter Unit (Thrusters)
+        #region New_Item
+            newPropulsion = new Propulsion();
+            newPropulsion.propulsionType = Propulsion.partType.Fighter_Unit;
+            newPropulsion.partName = "Standard Fighter Unit";
+            parts.Add(newPropulsion);
+        #endregion
+
+        // --- Hover Unit (Slower, Kinda Ion Engine types... )
+        #region New_Item
+            newPropulsion = new Propulsion();
+            newPropulsion.propulsionType = Propulsion.partType.Hover_Unit;
+            newPropulsion.partName = "Standard Hover Unit";
+            parts.Add(newPropulsion);
+        #endregion
+
+        #endregion
+
+        // ------------ Utility Parts
+        newUtility = new Utility();
+
+        parts.Add(newUtility);
+
+        #region Weapon_Parts
+        // ------------ Weapon Parts
+        // --- Legs
+        #region New_Item
+        newWeapon = new Weapon();
+        newWeapon.weaponType = Weapon.partType.Ballistic_Cannon;
+
+        parts.Add(newWeapon);
+        #endregion
+
+        #endregion
+
+        // ------------ Manipulation Parts
+        newManipulation = new Manipulation();
+
+        parts.Add(newManipulation);
+    }
+
+    // Generate player
+    void GeneratePlayer()
+    {
+        player = new Player();
+        player.EquipItem("Standard Engine");
+        player.EquipItem("Standard Legs");
+    }
+
+}
