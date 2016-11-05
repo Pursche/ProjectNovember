8903744df4ed8f504faed5f2821217db8ed0e960
Pursche <pursche01@gmail.com>
2016-11-06 00:08:19 +0100

    Add scenes and mainmenu

---
+++ Assets/Scripts/GUI/OnClick/ChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : IOnButtonClick
+{
+    public string sceneName;
+    public LoadSceneMode loadMode = LoadSceneMode.Single;
+
+    public override void OnClick()
+    {
+        SceneManager.LoadScene(sceneName, loadMode);
+    }
+}
